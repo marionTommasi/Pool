706177db76a56ca33242aa6607830451d46074eb
marionTommasi <marion.tommasi@etudiant.univ-lille1.fr>
2014-10-11 12:43:53 +0200

    Update ResourcePool.java

--- Pool/ResourcePool.java
+++ Pool/ResourcePool.java
@@ -6,11 +6,23 @@ import java.util.List;
 
 import pool.exception.NoSuchElementException;
 
+/**
+ * Abstract class to manage resources. 
+ * 
+ * @author Marion Tommasi
+ *
+ * @param <R> a type of resource
+ */
 public abstract class ResourcePool<R extends Resource> {
 	
 	private List<R> stock;
 	private List<R> busy;
 	
+	/**
+	 * Constructor of ResourcePool.
+	 * 
+	 * @param nbResources the number of resources to manage
+	 */
 	public ResourcePool(int nbResources) {
 		this.stock = new ArrayList<R>(this.CreateResource(nbResources));
 		this.busy = new ArrayList<R>(nbResources);
@@ -18,6 +30,12 @@ public abstract class ResourcePool<R extends Resource> {
 
 	protected abstract Collection<R> CreateResource(int nbResources);
 
+	/**
+	 * Allocate a resource.
+	 * 
+	 * @return the resource allocated
+	 * @throws NoSuchElementException if there is no resource to allocate
+	 */
 	public R provideResources() throws NoSuchElementException{
 		if (this.stock.isEmpty()) {
 			throw new NoSuchElementException();
@@ -30,6 +48,13 @@ public abstract class ResourcePool<R extends Resource> {
 		}
 	}
 
+	/**
+	 * Take back a resource.
+	 * The resouce must be one previously allocated by the current ResourcePool. 
+	 * 
+	 * @param resource the resource passed back
+	 * @throws IllegalArgumentException if the resource dosn't come from the current ResourcePool
+	 */
 	public void FreeResource(R resource) throws IllegalArgumentException{
 		if (this.busy.contains(resource)) {
 			this.busy.remove(resource);
